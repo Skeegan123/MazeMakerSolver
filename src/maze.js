9bd391073a37a056a2474a3738b06ac3ff5f696e
Skeegan123 <mcfeegan123@gmail.com>
2021-11-13 04:15:59 -0700

    Finished maze making algorithm

---
+++ src/maze.js
@@ -0,0 +1,195 @@
+let maze = document.querySelector(".maze");
+let ctx = maze.getContext("2d");
+
+let current;
+
+class Maze {
+    constructor(rows, columns) {
+        this.vertSize = rows * 25;
+        this.horizSize = columns * 25;
+        this.columns = columns;
+        this.rows = rows;
+        this.grid = [];
+        this.stack = [];
+    }
+
+    setup() {
+        for (let r = 0; r < this.rows; r++) {
+            let row = [];
+            for (let c = 0; c < this.columns; c++) {
+                let cell = new Cell(r, c, this.grid, this.vertSize, this.horizSize);
+                row.push(cell);
+            }
+            this.grid.push(row);
+        }
+        current = this.grid[0][0];
+    }
+
+    draw() {
+        maze.width = this.horizSize;
+        maze.height = this.vertSize;
+        maze.style.background = "black";
+        current.visited = true;
+
+        for (let r = 0; r < this.rows; r++) {
+            for (let c = 0; c < this.columns; c++) {
+                let grid = this.grid;
+                grid[r][c].show(this.vertSize, this.horizSize, this.rows, this.columns)
+            }
+        }
+
+        let next = current.checkNeighbors();
+
+        if (next) {
+            next.visited = true;
+
+            this.stack.push(current);
+
+            current.highlight(this.rows, this.columns);
+
+            current.removeWalls(current, next)
+
+            current = next;
+        } else if (this.stack.length > 0) {
+            let cell = this.stack.pop();
+            current = cell;
+            current.highlight(this.rows, this.columns)
+        }
+
+        if (this.stack.length === 0) {
+            return;
+        }
+
+        window.requestAnimationFrame(() => {
+            this.draw();
+        })
+    }
+}
+
+class Cell {
+    constructor(rowNum, colNum, parentGrid, parentVertSize, parentHorizSize) {
+        this.rowNum = rowNum;
+        this.colNum = colNum;
+        this.parentGrid = parentGrid;
+        this.parentVertSize = parentVertSize;
+        this.parentHorizSize = parentHorizSize;
+        this.visited = false;
+        this.walls = {
+            topWall : true,
+            rightWall : true,
+            bottomWall : true,
+            leftWall : true
+        }
+    }
+
+    checkNeighbors() {
+        let grid = this.parentGrid;
+        let row = this.rowNum;
+        let col = this.colNum;
+        let neighbours = [];
+
+        let top = row !== 0 ? grid[row - 1][col] : undefined;
+        let right = col !== grid.length - 1 ? grid[row][col + 1] : undefined;
+        let bottom = row !== grid[0].length - 1 ? grid[row + 1][col] : undefined;
+        let left = col !== 0 ? grid[row][col - 1] : undefined;
+
+        if (top && !top.visited) neighbours.push(top);
+        if (right && !right.visited) neighbours.push(right);
+        if (bottom && !bottom.visited) neighbours.push(bottom);
+        if (left && !left.visited) neighbours.push(left);
+        
+        if (neighbours.length !== 0) {
+            let random = Math.floor(Math.random() * neighbours.length);
+            return neighbours[random];
+        } else {
+            return undefined;
+        }
+    }
+
+    drawTopWall(x, y, vertSize, horizSize, columns, rows) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+        ctx.lineTo(x + horizSize / columns, y);
+        ctx.stroke();
+    }
+
+    drawRightWall(x, y, vertSize, horizSize, columns, rows) {
+        ctx.beginPath();
+        ctx.moveTo(x + horizSize / columns, y);
+        ctx.lineTo(x + horizSize / columns, y + vertSize / rows);
+        ctx.stroke();
+    }
+
+    drawBottomWall(x, y, vertSize, horizSize, columns, rows) {
+        ctx.beginPath();
+        ctx.moveTo(x, y + vertSize / rows);
+        ctx.lineTo(x + horizSize / columns, y + vertSize / rows);
+        ctx.stroke();
+    }
+
+    drawLeftWall(x, y, vertSize, horizSize, columns, rows) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+        ctx.lineTo(x, y + vertSize / rows);
+        ctx.stroke();
+    }
+
+    highlight(rows, columns) {
+        let x = (this.colNum * this.parentHorizSize) / columns + 1;
+        let y = (this.rowNum * this.parentVertSize) / rows + 1;
+
+        ctx.fillStyle = "purple";
+        ctx.fillRect(x, y, this.parentHorizSize / columns - 1, this.parentVertSize / rows - 3)
+    }
+
+    removeWalls(cell1, cell2) {
+        let x = (cell1.colNum - cell2.colNum);
+
+        if (x === 1) {
+            cell1.walls.leftWall = false;
+            cell2.walls.rightWall = false;
+        } else if (x === -1) {
+            cell1.walls.rightWall = false;
+            cell2.walls.leftWall = false;
+        }
+
+        let y = (cell1.rowNum - cell2.rowNum);
+
+        if (y === 1) {
+            cell1.walls.topWall = false;
+            cell2.walls.bottomWall = false;
+        } else if (y === -1) {
+            cell1.walls.bottomWall = false;
+            cell2.walls.topWall = false;
+        }
+    }
+
+    show(vertSize, horizSize, rows, columns) {
+        let x = (this.colNum * horizSize) / columns;
+        let y = (this.rowNum * vertSize) / rows;
+
+        ctx.strokeStyle = "white";
+        ctx.fillStyle = "black";
+        ctx.lineWidth = 2;
+
+        if (this.walls.topWall) {
+            this.drawTopWall(x, y, vertSize, horizSize, columns, rows);
+        }
+        if (this.walls.rightWall) {
+            this.drawRightWall(x, y, vertSize, horizSize, columns, rows);
+        }
+        if (this.walls.bottomWall) {
+            this.drawBottomWall(x, y, vertSize, horizSize, columns, rows);
+        }
+        if (this.walls.leftWall) {
+            this.drawLeftWall(x, y, vertSize, horizSize, columns, rows);
+        }
+        if (this.visited) {
+            ctx.fillRect(x + 1, y + 1, horizSize / columns - 2, vertSize / rows - 2);
+        }
+    }
+}
+
+let newMaze = new Maze(25, 25);
+newMaze.setup();
+newMaze.draw();
